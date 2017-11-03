14dee563d48c98fd12f0595af141020e1166287d
Sebastian Gilits <sep.gil@gmail.com>
2017-11-03 17:02:21 +0100

    Update config to aurora changes.

--- aurora/local_settings.py
+++ aurora/local_settings.py
@@ -14,13 +14,38 @@ LECTURER_SECRET = 'lecturersecret'
 
 SSO_SHARED_SECRET = 'ssosecret'
 
+AUTHENTICATION_BACKENDS = (
+  'middleware.AuroraAuthenticationBackend.AuroraAuthenticationBackend',
+  'django.contrib.auth.backends.ModelBackend',
+)
+
+ALLOWED_HOSTS = ['aurora.dork.io']
+
+CHANNEL_LAYERS = {
+  "default": {
+    "BACKEND": "asgi_redis.RedisChannelLayer",
+    "CONFIG": {
+      "hosts": [("redis", 6379)],
+    },
+    "ROUTING": "AuroraProject.routing.channel_routing",
+  }
+}
+
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'aurora',
-    'USER': 'aurora',
+    'USER': 'aurora_dbuser',
     'PASSWORD': 'nosecret',
-    'HOST': 'database',
-    'PORT': '5432',
-  }
+    'HOST': 'localhost',
+    'PORT': '',
+  },
+  'plagcheck': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'plagcheck',
+    'USER': 'aurora_dbuser',
+    'PASSWORD': 'nosecret',
+    'HOST': 'localhost',
+    'PORT': '',
+  },
 }
